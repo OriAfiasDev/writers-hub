f417ae5c4ceadccc57d61fc96e567748d8147318
Ori Afias <ori@afias.dev>
2024-01-13 19:11:31 +0200

    hover theme switcher

--- src/components/ui/ThemeSwitcher.tsx
+++ src/components/ui/ThemeSwitcher.tsx
@@ -8,7 +8,7 @@ export default function ThemeSwitcher() {
 
   return (
     <div
-      className='flex items-center justify-center w-fit h-10 rounded-lg bg-gradient cursor-pointer px-2 shadow-lg'
+      className='flex items-center justify-center w-fit h-10 rounded-lg btn-gradient cursor-pointer px-2 shadow-lg'
       onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
     >
       <CiDark className='text-2xl text-white rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
