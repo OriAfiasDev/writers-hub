d4edfe5bfd543d87bdb7f43f49af57935dab4944
Ori Afias <ori@afias.dev>
2024-01-13 17:53:44 +0200

    style tweaks

--- src/app/wordcount/page.tsx
+++ src/app/wordcount/page.tsx
@@ -8,28 +8,22 @@ import TextArea from '@/components/ui/TextArea';
 export default function WordCounter() {
   const [text, setText] = useState('');
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='flex flex-col items-center  border rounded-lg bg-white  w-10/12 h-4/5 p-6'>
+    <main className='flex flex-col items-center justify-center p-24'>
+      <div className='flex flex-col items-center rounded-lg bg-primary w-10/12 h-full p-6'>
         <StatsToolbar text={text} />
 
         <hr className='divider' />
 
-        <TextArea placeholder='Type or paste your text here.' value={text} setValue={setText} />
+        <TextArea
+          placeholder='Type or paste your text here.'
+          value={text}
+          setValue={setText}
+          className='w-full h-[480px]'
+        />
 
         <hr className='divider' />
 
         <SocialToolbar text={text} />
-        {/* <hr className='divider' />
-        <div id='times' className='flex items-center justify-evenly w-full'>
-          <div className='flex flex-col'>
-            <h2 className='text-5xl'>12s</h2>
-            <p className='text-md uppercase'>reading time</p>
-          </div>
-          <div className='flex flex-col'>
-            <h2 className='text-5xl'>16s</h2>
-            <p className='text-md uppercase'>speaking time</p>
-          </div>
-        </div> */}
       </div>
     </main>
   );
