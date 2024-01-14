27b0b4b2cb9571c03b034411a21057eca24290ca
Ori Afias <ori@afias.dev>
2024-01-14 09:34:46 +0200

    editing toolbar mobile

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -10,7 +10,7 @@ export default function IconButton({ icon, onClick, className, title, disabled }
   return (
     <span
       aria-disabled={disabled}
-      className={`h-8 w-8 lg:h-12 lg:w-12 mx-2 lg:mx-0 lg:my-2' flex justify-center items-center bg-secondary cursor-pointer rounded-md ${className} has-tooltip ${
+      className={`h-8 w-8 lg:h-12 lg:w-12 mx-2 lg:mx-0 lg:my-2 flex justify-center items-center bg-secondary cursor-pointer rounded-md ${className} has-tooltip ${
         disabled && 'pointer-events-none opacity-50'
       }`}
       onClick={onClick}
